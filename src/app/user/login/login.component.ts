0c542420b7aefcdc9b53cdb65f9fd89dc1ef8ffc
TheBigMouse-MernStack <khanhnqse19@gmail.com>
2025-05-31 21:19:53 +0700

    login validate form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { GoogleComponent } from '../google/google.component';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,15 +17,47 @@ import { GoogleComponent } from '../google/google.component';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  enteredEmail = '';
-  enterdPassword = '';
+  enteredEmailOrPhone = '';
+  enteredPassword = '';
   RememberMe = false;
   ShowPass = false;
+  formSubmitted = false;
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
 
-  onSubmit(form: NgForm) {
-    console.log('Email:', this.enteredEmail);
-    console.log('Password:', this.enterdPassword);
-    // Hoặc dùng object:
-    // console.log({ email: this.email, password: this.password });
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('save-login-form');
+
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmailOrPhone = loadedFormData.emailOrPhone;
+        Promise.resolve().then(() => {
+          const control = this.form().controls['emailOrPhone'];
+          if (control) {
+            control.setValue(savedEmailOrPhone);
+          }
+        });
+      }
+
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'save-login-form',
+              JSON.stringify({ emailOrPhone: value.emailOrPhone })
+            ),
+        });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
+  onSubmit(formData: NgForm) {
+    this.formSubmitted = true;
+    if (!formData.valid) return; // Không submit nếu chưa hợp lệ
+    // Thực hiện đăng nhập ở đây, ví dụ gọi API
+    // ...
+    formData.reset();
   }
 }
